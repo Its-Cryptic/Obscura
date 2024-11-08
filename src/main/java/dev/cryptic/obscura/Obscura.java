90898ea805b4568e6284cfdee52e54426945aeef
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2024-11-07 21:50:29 -0800

    Delete Everything

--- src/main/java/dev/cryptic/obscura/Obscura.java
+++ src/main/java/dev/cryptic/obscura/Obscura.java
@@ -1,37 +1,12 @@
 package dev.cryptic.obscura;
 
-import dev.cryptic.obscura.core.EngineManager;
-import dev.cryptic.obscura.core.Window;
-import dev.cryptic.obscura.core.utils.WindowSizePresets;
-import dev.cryptic.obscura.test.TestGame;
-
 import org.apache.logging.log4j.*;
-
-import static dev.cryptic.obscura.core.utils.Consts.*;
 
 public class Obscura {
     public static final Logger LOGGER = LogManager.getLogger(Obscura.class.getSimpleName());
-    private static Window window;
-    private static TestGame game;
 
     public static void main(String[] args) {
         LOGGER.error("Starting Obscura");
-        window = new Window(TITLE, WindowSizePresets.HD, true);
-        game = new TestGame();
-        EngineManager engine = new EngineManager();
-
-        try {
-            engine.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    public static Window getWindow() {
-        return window;
-    }
-
-    public static TestGame getGame() {
-        return game;
-    }
 }
--- src/main/java/dev/cryptic/obscura/core/utils/Consts.java
+++
@@ -1,7 +0,0 @@
-package dev.cryptic.obscura.core.utils;
-
-public class Consts {
-    public static final String TITLE = "Obscura";
-    public static final String VERSION = "0.1.0";
-
-}
